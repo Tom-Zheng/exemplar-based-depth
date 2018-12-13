d5d3d14dadac176fffa42a5955bb615738252609
Zheng Tian <t-zheng@outlook.com>
2018-12-13 15:18:35 +0800

    MEX modified.

--- bestexemplarhelper.c
+++ bestexemplarhelper.c
@@ -12,15 +12,16 @@
 // (m,n) : size of Ip; (mm,nn):size of the entire image; img, Ip, toFill: mask for Ip, to be filled; sourceRegion for 
 void bestexemplarhelper(const int mm, const int nn, const int m, const int n, 
 			const double *img, const double *Ip, 
+                        const double *depth, const double *Dp,
 			const mxLogical *toFill, const mxLogical *sourceRegion,
 			double *best) 
 {
   register int i,j,ii,jj,ii2,jj2,M,N,I,J,ndx,ndx2,mn=m*n,mmnn=mm*nn;
   double patchErr=0.0,err=0.0,bestErr=1000000000.0;
-
+  const double alpha = 1.0;
   /* foreach patch */
-  N=nn-n+1;  M=mm-m+1;
-  for (j=1; j<=N; ++j) {
+  N=nn-n+1;  M=mm-m+1;    // Search window boundary.
+  for (j=1; j<=N; ++j) {  // For each column..
     J=j+n-1;
     for (i=1; i<=M; ++i) {
       I=i+m-1;
@@ -28,13 +29,15 @@ void bestexemplarhelper(const int mm, const int nn, const int m, const int n,
       /* foreach pixel in the current patch */
       for (jj=j,jj2=1; jj<=J; ++jj,++jj2) {
 	for (ii=i,ii2=1; ii<=I; ++ii,++ii2) {
-	  ndx=ii-1+mm*(jj-1);
+	  ndx=ii-1+mm*(jj-1);                        // Notice the array arrangemeent is Fortran style.
 	  if (!sourceRegion[ndx])
 	    goto skipPatch;
 	  ndx2=ii2-1+m*(jj2-1);
 	  if (!toFill[ndx2]) {
 	    err=img[ndx      ] - Ip[ndx2    ]; patchErr += err*err;
+	    err=depth[ndx      ] - depth[ndx2    ]; patchErr += alpha*err*err;
 	    err=img[ndx+=mmnn] - Ip[ndx2+=mn]; patchErr += err*err;
+	    err=depth[ndx] - depth[ndx2]; patchErr += alpha*err*err;
 	    err=img[ndx+=mmnn] - Ip[ndx2+=mn]; patchErr += err*err;
 	  }
 	}
@@ -57,17 +60,20 @@ void mexFunction(int nlhs,mxArray *plhs[],int nrhs,const mxArray *prhs[])
 {
   int mm,nn,m,n;
   double *img,*Ip,*best;
+  double *depth, *Dp;
   mxLogical *toFill,*sourceRegion;
 
   /* Extract the inputs */
-  mm = (int)mxGetScalar(prhs[0]);
-  nn = (int)mxGetScalar(prhs[1]);
-  m  = (int)mxGetScalar(prhs[2]);
-  n  = (int)mxGetScalar(prhs[3]);
-  img = mxGetPr(prhs[4]);
-  Ip  = mxGetPr(prhs[5]);
-  toFill = mxGetLogicals(prhs[6]);
-  sourceRegion = mxGetLogicals(prhs[7]);
+  mm = (int)mxGetScalar(prhs[0]);    // (mm, nn) size of image.
+  nn = (int)mxGetScalar(prhs[1]);    // 
+  m  = (int)mxGetScalar(prhs[2]);    // (m, n) size of patch.
+  n  = (int)mxGetScalar(prhs[3]);    //
+  img = mxGetPr(prhs[4]);            // Image data.
+  Ip  = mxGetPr(prhs[5]);            // Image patch.
+  depth = mxGetPr(prhs[6]);          // Depth Gradient data.
+  Dp  = mxGetPr(prhs[7]);            // Depth Gradient patch.
+  toFill = mxGetLogicals(prhs[8]);
+  sourceRegion = mxGetLogicals(prhs[9]);
   
   /* Setup the output */
   plhs[0] = mxCreateDoubleMatrix(4,1,mxREAL);
@@ -75,5 +81,5 @@ void mexFunction(int nlhs,mxArray *plhs[],int nrhs,const mxArray *prhs[])
   best[0]=best[1]=best[2]=best[3]=0.0;
 
   /* Do the actual work */
-  bestexemplarhelper(mm,nn,m,n,img,Ip,toFill,sourceRegion,best);
+  bestexemplarhelper(mm,nn,m,n,img,Ip,depth, Dp,toFill,sourceRegion,best);
 }
